14acee1561f9e8c506e2f6557f6715c2ca3b60fd
yatr0316 <yauheni.tratseuski@netcracker.com>
2024-11-23 10:56:06 +0300

    yarn repo sample

---
+++ package_managers/npx-workspace/apps/app2/src/app/app.tsx
@@ -0,0 +1,15 @@
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import styles from './app.module.less';
+import NxWelcome from './nx-welcome';
+// import * as sharedUtils from '../../../../apps/libs/shared-utils/src/index' ;
+
+export function App() {
+  return (
+    <div>
+      <NxWelcome title="app2" />
+      sharedUtils.
+    </div>
+  );
+}
+
+export default App;
---
+++ package_managers/npx-workspace/apps/libs/shared-utils/src/lib/shared-utils.tsx
@@ -0,0 +1,11 @@
+import styles from './shared-utils.module.less';
+
+export function SharedUtils() {
+  return (
+    <div className={styles['container']}>
+      <h1>Welcome to SharedUtils!</h1>
+    </div>
+  );
+}
+
+export default SharedUtils;
